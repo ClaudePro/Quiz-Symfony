88a1242b7d657fceeb2caac71657c12feaa2d772
Duke <claude-facebook@hotmail.fr>
2018-05-25 11:45:26 +0200

    css

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -6,6 +6,8 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
+use AppBundle\Entity\Categorie;
+
 class DefaultController extends Controller
 {
     /**
@@ -17,5 +19,31 @@ class DefaultController extends Controller
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
+    }
+
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function renderQuiz(Request $request)
+    {
+        $categorie = $this->getDoctrine()->getManager()
+        ->getRepository(Categorie::class)
+        ->findAll();
+
+        return $this->render('default/index.html.twig', array('data' => $categorie));
+
+        if (!$categorie) {
+            throw $this->createNotFoundException(
+                'No product found for id '.$categorieId
+            );
+        }
+    }
+
+    /**
+     * @Route("/create/quiz", name="create")
+     */
+    public function createQuiz(Request $request)
+    {
+        return $this->render('default/create.html.twig');
     }
 }
